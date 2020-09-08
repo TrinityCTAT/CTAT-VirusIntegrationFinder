feb10e691d188c3d2d3dbf92869cd5a64f749e8f
Brian Haas <brianjohnhaas@gmail.com>
2020-09-08 14:32:56 -0400

    update

--- util/misc/padded_merged_contig_evidence_analyzer.py
+++ util/misc/padded_merged_contig_evidence_analyzer.py
@@ -7,12 +7,11 @@ import subprocess
 import math
 import pysam
 from collections import defaultdict
+import csv
 
 if sys.version_info[0] < 3:
     raise Exception("This script requires Python 3")
 
-sys.path.insert(0, os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), "../PyLib"]))
-from Pipeliner import Pipeliner, Command
 
 import logging
 logger = logging.getLogger(__name__)
@@ -24,23 +23,60 @@ logging.basicConfig(level=logging.INFO,
 
 def main():
     
-    arg_parser = argparse.ArgumentParser(description="runs STAR, gathering chimeric junctions",
-                                        formatter_class=argparse.RawTextHelpFormatter
+    arg_parser = argparse.ArgumentParser(description="counts spanning and split evidence reads\n" +
+                                         "two run modes:\n" +
+                                         " - use a single bam and gtf\n" +
+                                         " or \n" +
+                                         " - give a partitioned data directory and it will iterate through all entries.\n\n",
+                                         formatter_class=argparse.RawTextHelpFormatter
                                         )
+    
 
-
-    arg_parser.add_argument("--patch_db_bam", type=str, required=True,
+    arg_parser.add_argument("--patch_db_bam", type=str, required=False, default=None,
                             help="patch genome aligned bam")
 
-    arg_parser.add_argument("--patch_db_gtf", type=str, required=True,
+    arg_parser.add_argument("--patch_db_gtf", type=str, required=False, default=None,
                             help="patch gtf")
+
+    arg_parser.add_argument("--partitioned_dir", type=str, required=False, default=None,
+                            help="partitioned data directory, expect to find: 'chim_events_for_eval.tsv' in that dir.")
 
 
     args = arg_parser.parse_args()
 
     bam_file = args.patch_db_bam
     gtf_file = args.patch_db_gtf
+    partitioned_dir = args.partitioned_dir
 
+    if not ((bam_file != None and gtf_file != None) ^ (partitioned_dir != None)):
+        arg_parser.print_help()
+        sys.exit(1)
+
+
+    
+    print("\t".join(["contig", "split", "span", "total"]))  # report header
+    
+    if partitioned_dir:
+        chim_events_file = os.path.join(partitioned_dir, "chim_events_for_eval.tsv")
+        if not os.path.exists(chim_events_file):
+            raise RuntimeError("Error, cannot locate expected file: {}".format(chim_events_file))
+        with open(chim_events_file) as fh:
+            csv_reader = csv.DictReader(fh, delimiter="\t")
+            for row in csv_reader:
+                workdir = row['workdir']
+                target_bam = os.path.join(workdir, "Aligned.sortedByCoord.out.bam")
+                target_gtf = os.path.join(workdir, "target.gtf")
+                analyze_bam_n_gtf(target_bam, target_gtf)
+        
+    else:
+        analyze_bam_n_gtf(bam_file, gtf_file)
+
+
+    sys.exit(0)
+
+        
+def analyze_bam_n_gtf(bam_file, gtf_file):
+    
     contig_to_region_pair_dict = parse_region_pairs_from_gtf(gtf_file)
     
     samfile = pysam.AlignmentFile(bam_file, "rb")
@@ -83,8 +119,7 @@ def main():
     ## count'em up
     sorted_contigs = sorted(init_contig_support.keys(), key=lambda x: init_contig_support[x], reverse=True)
 
-    print("\t".join(["contig", "split", "span", "total"]))
-    
+        
     read_seen = set()
     for contig in sorted_contigs:
         type_counter = defaultdict(int)
@@ -99,9 +134,8 @@ def main():
         num_total = num_split + num_span
     
         print("\t".join([contig, str(num_split), str(num_span), str(num_total)]))
-        
-            
-    sys.exit(0)
+
+    return
 
 
 
