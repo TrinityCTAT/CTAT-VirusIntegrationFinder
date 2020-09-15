c47b8afcf814f2be9fb0861b098a2337f1bdf1e1
Brian Haas <brianjohnhaas@gmail.com>
2020-09-15 19:14:41 +0000

    heavily refined

--- util/chimJ_to_virus_insertion_candidate_sites.py
+++ util/chimJ_to_virus_insertion_candidate_sites.py
@@ -9,13 +9,15 @@ from collections import defaultdict
 if sys.version_info[0] < 3:
     raise Exception("This script requires Python 3")
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s : %(levelname)s : %(message)s',
-                    datefmt='%H:%M:%S')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s : %(levelname)s : %(message)s",
+    datefmt="%H:%M:%S",
+)
 logger = logging.getLogger(__name__)
 
 
-'''  Chimeric.out.junction file formatting from the STAR manual:
+"""  Chimeric.out.junction file formatting from the STAR manual:
 
         # from star doc:
         #The rst 9 columns give information about the chimeric junction:
@@ -40,30 +42,51 @@ logger = logging.getLogger(__name__)
         #column 12: CIGAR of the rst segment
         #column 13: rst base of the second segment
         #column 14: CIGAR of the second segment
-'''
-
+"""
 
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Defines candidate virus insertion sites based on chimeric reads",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    
-    parser.add_argument("--chimJ", type=str, required=True, help="STAR Chimeric.out.junction file")
-    
-    parser.add_argument("--patch_db_fasta", type=str, required=True,
-                        help="database of the additional genome targets")
+    parser = argparse.ArgumentParser(
+        description="Defines candidate virus insertion sites based on chimeric reads",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
+    parser.add_argument(
+        "--chimJ", type=str, required=True, help="STAR Chimeric.out.junction file"
+    )
 
-    parser.add_argument("--aggregation_dist", type=int, required=False, default=500,
-                        help="distance around top chimeric event breakpoint for aggregating supporting reads")
+    parser.add_argument(
+        "--patch_db_fasta",
+        type=str,
+        required=True,
+        help="database of the additional genome targets",
+    )
 
-    parser.add_argument("--output_prefix", "-o", dest="output_prefix", type=str, required=True,
-                        help = "output prefix")
+    parser.add_argument(
+        "--aggregation_dist",
+        type=int,
+        required=False,
+        default=500,
+        help="distance around top chimeric event breakpoint for aggregating supporting reads",
+    )
 
-    parser.add_argument("--remove_duplicates_flag", action='store_true', default=False,
-                        help="exclude duplicate alignments")
-    
+    parser.add_argument(
+        "--output_prefix",
+        "-o",
+        dest="output_prefix",
+        type=str,
+        required=True,
+        help="output prefix",
+    )
+
+    parser.add_argument(
+        "--remove_duplicates_flag",
+        action="store_true",
+        default=False,
+        help="exclude duplicate alignments",
+    )
+
     args_parsed = parser.parse_args()
 
     chimJ_filename = args_parsed.chimJ
@@ -72,78 +95,78 @@ def main():
     output_prefix = args_parsed.output_prefix
 
     remove_duplicates_flag = args_parsed.remove_duplicates_flag
-    
+
     ## get list of patch_db entries.
     patch_db_entries = set()
-    with open(patch_db_fasta_filename, 'rt') as fh:
+    with open(patch_db_fasta_filename, "rt") as fh:
         for line in fh:
             m = re.search("^>(\S+)", line)
             if m:
                 acc = m.group(1)
                 patch_db_entries.add(acc)
 
-    
-    junction_type_encoding = { "-1" : "Span",
-                               "0" : "Split",
-                               "1" : "Split", #GT/AG",
-                               "2" : "Split" } #CT/AC" }
+    junction_type_encoding = {
+        "-1": "Span",
+        "0": "Split",
+        "1": "Split",  # GT/AG",
+        "2": "Split",
+    }  # CT/AC" }
 
-    opposite_orientation = { '+' : '-',
-                             '-' : '+' }
-
+    opposite_orientation = {"+": "-", "-": "+"}
 
     genome_pair_to_evidence = defaultdict(set)
-    
 
     duplicate_read_catcher = set()
-    
-    with open(chimJ_filename, 'rt') as fh:
+
+    with open(chimJ_filename, "rt") as fh:
         for line in fh:
             line = line.rstrip()
             vals = line.split("\t")
-            junction_type = junction_type_encoding[ vals[6] ]
+            junction_type = junction_type_encoding[vals[6]]
             readname = vals[9]
-            (chrA, coordA, orientA) = (vals[0], vals[1], vals[2]);
-            (chrB, coordB, orientB) = (vals[3], vals[4], vals[5]);
+            (chrA, coordA, orientA) = (vals[0], vals[1], vals[2])
+            (chrB, coordB, orientB) = (vals[3], vals[4], vals[5])
 
             coordA = int(coordA)
             coordB = int(coordB)
-            
+
             if not (chrA in patch_db_entries) ^ (chrB in patch_db_entries):
                 # must involve just the host genome and a patch db entry
                 continue
 
-
             token = "^".join(vals[0:6] + vals[10:])
             hashed_token = hash(token)
-            
-            #print("{} -> {}".format(hashed_token, token))
+
+            # print("{} -> {}".format(hashed_token, token))
             if remove_duplicates_flag:
                 if hashed_token in duplicate_read_catcher:
                     logger.info("-DUPLICATE: {}".format(line))
                     continue
                 else:
                     duplicate_read_catcher.add(hashed_token)
-            
-            
-            ## reorient so host genome is always in the + reference orientation.
-            if (
-                (chrA in patch_db_entries and  orientB == '-')
-                or
-                (chrB in patch_db_entries and orientA == '-') ):
-                            
-                # swap them so that the host chr shows up in + orient.
-                (chrA, coordA, orientA,
-                 chrB, coordB, orientB) = (chrB, coordB, opposite_orientation[orientB],
-                                           chrA, coordA, opposite_orientation[orientA] )
 
-            
-            chim_read = Chimeric_read(chrA, coordA, orientA, chrB, coordB, orientB, junction_type, readname)
+            ## reorient so host genome is always in the + reference orientation.
+            if (chrA in patch_db_entries and orientB == "-") or (
+                chrB in patch_db_entries and orientA == "-"
+            ):
+
+                # swap them so that the host chr shows up in + orient.
+                (chrA, coordA, orientA, chrB, coordB, orientB) = (
+                    chrB,
+                    coordB,
+                    opposite_orientation[orientB],
+                    chrA,
+                    coordA,
+                    opposite_orientation[orientA],
+                )
+
+            chim_read = Chimeric_read(
+                chrA, coordA, orientA, chrB, coordB, orientB, junction_type, readname
+            )
 
             genome_pair = "^".join([chrA, chrB])
 
             genome_pair_to_evidence[genome_pair].add(chim_read)
-
 
     all_chim_events = list()
 
@@ -159,48 +182,112 @@ def main():
 
         all_chim_events.extend(chim_events)
 
-
     ## prioritize by total read support.
-    all_chim_events = sorted(all_chim_events, key=lambda x: x.get_read_support()[2], reverse=True)
-    
+    all_chim_events = sorted(
+        all_chim_events, key=lambda x: x.get_read_support()[2], reverse=True
+    )
 
     ## #####################
     ## generate final report.
     output_filename_full = output_prefix + ".full.tsv"
     output_filename_abridged = output_prefix + ".abridged.tsv"
-    
-    output_filename_detailed_abridged = output_prefix + ".abridged.detailed.tsv"
-    ofh_detailed = open(output_filename_detailed_abridged, 'wt')
 
-    with open(output_filename_abridged, 'wt') as ofh:
-        with open(output_filename_full, 'wt') as ofh_full:
+    output_filename_detailed_abridged = output_prefix + ".abridged.detailed.tsv"
+    ofh_detailed = open(output_filename_detailed_abridged, "wt")
+
+    with open(output_filename_abridged, "wt") as ofh:
+        with open(output_filename_full, "wt") as ofh_full:
 
             # print header
-            ofh.write("\t".join(["entry", "chrA", "coordA", "orientA", "chrB", "coordB", "orientB", "primary_brkpt_type", "num_primary_reads", "num_supp_reads", "total_reads"]) + "\n") 
-            ofh_full.write("\t".join(["entry", "chrA", "coordA", "orientA", "chrB", "coordB", "orientB", "primary_brkpt_type", "num_primary_reads", "num_supp_reads", "total_reads", "readnames"]) + "\n") 
+            ofh.write(
+                "\t".join(
+                    [
+                        "entry",
+                        "chrA",
+                        "coordA",
+                        "orientA",
+                        "chrB",
+                        "coordB",
+                        "orientB",
+                        "primary_brkpt_type",
+                        "num_primary_reads",
+                        "num_supp_reads",
+                        "total",
+                    ]
+                )
+                + "\n"
+            )
+            ofh_full.write(
+                "\t".join(
+                    [
+                        "entry",
+                        "chrA",
+                        "coordA",
+                        "orientA",
+                        "chrB",
+                        "coordB",
+                        "orientB",
+                        "primary_brkpt_type",
+                        "num_primary_reads",
+                        "num_supp_reads",
+                        "total",
+                        "readnames",
+                    ]
+                )
+                + "\n"
+            )
 
-            ofh_detailed.write("\t".join(["entry", "chrA", "coordA", "orientA", "chrB", "coordB", "orientB", "primary_brkpt_type", "num_primary_reads"]) + "\n") 
-            
-            
-            chim_counter = 0
+            ofh_detailed.write(
+                "\t".join(
+                    [
+                        "parent_entry",
+                        "entry",
+                        "chrA",
+                        "coordA",
+                        "orientA",
+                        "chrB",
+                        "coordB",
+                        "orientB",
+                        "primary_brkpt_type",
+                        "num_primary_reads",
+                    ]
+                )
+                + "\n"
+            )
+
             for chim_event in all_chim_events:
-                chim_counter += 1
-                print(str(chim_counter) + "\t" + str(chim_event), file=ofh)
+                print(
+                    chim_event.get_event_accession() + "\t" + str(chim_event), file=ofh
+                )
                 supporting_reads = chim_event.get_readnames()
-                print(str(chim_counter) + "\t" + str(chim_event) + "\t" + ",".join(supporting_reads), file=ofh_full)
+                print(
+                    chim_event.get_event_accession()
+                    + "\t"
+                    + str(chim_event)
+                    + "\t"
+                    + ",".join(supporting_reads),
+                    file=ofh_full,
+                )
 
                 # detailed entry-level event report
+                parent_chim_event = chim_event
                 for event in [chim_event] + chim_event.chimeric_events_absorbed:
-                    print(str(chim_counter) + "\t" + event.get_coordstring() + "\t" + str(len(event.chimeric_reads_list)), file=ofh_detailed)
+                    print(
+                        parent_chim_event.get_event_accession()
+                        + "\t"
+                        + event.get_event_accession()
+                        + "\t"
+                        + event.get_coordstring()
+                        + "\t"
+                        + str(len(event.chimeric_reads_list)),
+                        file=ofh_detailed,
+                    )
 
-    
     ofh_detailed.close()
 
     logger.info("-wrote output to {}".format(output_filename_abridged))
-    
+
     sys.exit(0)
-
-
 
 
 def group_chim_reads_into_events(chim_reads_list, aggregation_dist):
@@ -213,27 +300,30 @@ def group_chim_reads_into_events(chim_reads_list, aggregation_dist):
 
         top_event_reads, remaining_reads = gather_top_event_reads(remaining_reads)
         chim_event = Chimeric_event(top_event_reads)
-        
+
         if not supplements_existing_event(chim_event, chim_events, aggregation_dist):
             chim_events.append(chim_event)
-            logger.info('-logging chimeric event: ' + str(chim_event))
+            logger.info("-logging chimeric event: " + str(chim_event))
 
     return chim_events
 
-    
+
 def supplements_existing_event(chim_event, chim_events_list, aggregation_dist):
 
     for prev_chim_event in chim_events_list:
 
-        if ( chim_event.orientA == prev_chim_event.orientA
-             and
-             abs(chim_event.coordA - prev_chim_event.coordA) <= aggregation_dist
-             and
-             chim_event.orientB == prev_chim_event.orientB
-             and
-             abs(chim_event.coordB - prev_chim_event.coordB) <= aggregation_dist ):
+        if (
+            chim_event.orientA == prev_chim_event.orientA
+            and abs(chim_event.coordA - prev_chim_event.coordA) <= aggregation_dist
+            and chim_event.orientB == prev_chim_event.orientB
+            and abs(chim_event.coordB - prev_chim_event.coordB) <= aggregation_dist
+        ):
 
-            logger.info("-adding {} as supplement to {}".format(str(chim_event), str(prev_chim_event)))
+            logger.info(
+                "-adding {} as supplement to {}".format(
+                    str(chim_event), str(prev_chim_event)
+                )
+            )
 
             prev_chim_event.absorb_nearby_chim_event(chim_event)
 
@@ -242,27 +332,26 @@ def supplements_existing_event(chim_event, chim_events_list, aggregation_dist):
     return False
 
 
-        
-
-
-
 def gather_top_event_reads(reads_list):
 
     # count reads according to breakpoint.
 
     brkpt_counter = defaultdict(int)
     brkpt_type = dict()
-    
+
     for read in reads_list:
         brkpt = "{}^{}".format(read.coordA, read.coordB)
         brkpt_counter[brkpt] += 1
         brkpt_type[brkpt] = read.splitType
-    
-    # prioritize split reads over spanning reads.
-    priority = { 'Split' : 1,
-                 'Span' : 0 }
 
-    sorted_brkpts = sorted(brkpt_counter.keys(), key=lambda x: (priority[brkpt_type[x]], brkpt_counter[x]), reverse=True)
+    # prioritize split reads over spanning reads.
+    priority = {"Split": 1, "Span": 0}
+
+    sorted_brkpts = sorted(
+        brkpt_counter.keys(),
+        key=lambda x: (priority[brkpt_type[x]], brkpt_counter[x]),
+        reverse=True,
+    )
 
     top_brkpt = sorted_brkpts[0]
 
@@ -277,17 +366,12 @@ def gather_top_event_reads(reads_list):
             remaining_reads.append(read)
 
     return top_event_reads, remaining_reads
-    
-
-
-
-
-
 
 
 class Chimeric_read:
-
-    def __init__(self, chrA, coordA, orientA, chrB, coordB, orientB, splitType, readname=None):
+    def __init__(
+        self, chrA, coordA, orientA, chrB, coordB, orientB, splitType, readname=None
+    ):
         self.chrA = chrA
         self.coordA = coordA
         self.orientA = orientA
@@ -296,31 +380,42 @@ class Chimeric_read:
         self.orientB = orientB
         self.splitType = splitType
         self.readname = readname
-    
-        
+
     def __repr__(self):
-        return "\t".join([self.chrA, str(self.coordA), self.orientA, self.chrB, str(self.coordB), self.orientB, self.splitType])
+        return "\t".join(
+            [
+                self.chrA,
+                str(self.coordA),
+                self.orientA,
+                self.chrB,
+                str(self.coordB),
+                self.orientB,
+                self.splitType,
+            ]
+        )
 
-    
 
-class Chimeric_event (Chimeric_read):
-
-
+class Chimeric_event(Chimeric_read):
     def __init__(self, chimeric_reads_list):
         self.chimeric_reads_list = chimeric_reads_list
 
-        self.chimeric_events_absorbed = list()  # for neighboring chim events that also support this primary event but are either spanning or split w/ different nearby brkpt
+        self.chimeric_events_absorbed = (
+            list()
+        )  # for neighboring chim events that also support this primary event but are either spanning or split w/ different nearby brkpt
 
         example_read = chimeric_reads_list[0]
-        super().__init__(example_read.chrA, example_read.coordA, example_read.orientA,
-                       example_read.chrB, example_read.coordB, example_read.orientB,
-                       example_read.splitType)
+        super().__init__(
+            example_read.chrA,
+            example_read.coordA,
+            example_read.orientA,
+            example_read.chrB,
+            example_read.coordB,
+            example_read.orientB,
+            example_read.splitType,
+        )
 
-        
-    
     def absorb_nearby_chim_event(self, chim_event):
         self.chimeric_events_absorbed.append(chim_event)
-        
 
     def get_read_support(self):
         num_chimeric_reads = len(self.chimeric_reads_list)
@@ -328,19 +423,40 @@ class Chimeric_event (Chimeric_read):
         num_absorbed_reads = 0
         for chim_event in self.chimeric_events_absorbed:
             num_absorbed_reads += len(chim_event.chimeric_reads_list)
-        
+
         num_total_reads = num_chimeric_reads + num_absorbed_reads
 
         return num_chimeric_reads, num_absorbed_reads, num_total_reads
 
     def __repr__(self):
-        num_chimeric_reads, num_absorbed_reads, num_total_reads = self.get_read_support()
-        return(super().__repr__() + "\t{}\t{}\t{}".format(num_chimeric_reads, num_absorbed_reads, num_total_reads))
+        (
+            num_chimeric_reads,
+            num_absorbed_reads,
+            num_total_reads,
+        ) = self.get_read_support()
+
+        return super().__repr__() + "\t{}\t{}\t{}".format(
+            num_chimeric_reads, num_absorbed_reads, num_total_reads
+        )
 
     def get_coordstring(self):
-        return(super().__repr__())
-    
-    
+        return super().__repr__()
+
+    def get_event_accession(self):
+        return "~".join(
+            [
+                str(x)
+                for x in (
+                    self.chrA,
+                    self.coordA,
+                    self.orientA,
+                    self.chrB,
+                    self.coordB,
+                    self.orientB,
+                )
+            ]
+        )
+
     def get_readnames(self):
         readnames = list()
 
@@ -351,6 +467,5 @@ class Chimeric_event (Chimeric_read):
         return readnames
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
