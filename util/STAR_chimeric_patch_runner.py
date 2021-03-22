1990f29d751da661afa9b9865eb9f16869a07168
jgould <jgould@broadinstitute.org>
2021-03-22 17:25:34 -0400

    Pass files to tasks instead of genome lib dir

--- util/STAR_chimeric_patch_runner.py
+++ util/STAR_chimeric_patch_runner.py
@@ -18,23 +18,22 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%H:%M:%S')
 
 def main():
-    
+
     arg_parser = argparse.ArgumentParser(description="runs STAR, gathering chimeric junctions",
                                          formatter_class=argparse.RawTextHelpFormatter
                                          )
     arg_parser._action_groups.pop()
     required = arg_parser.add_argument_group('required arguments')
     optional = arg_parser.add_argument_group('optional arguments')
-    
+
     required.add_argument("--left_fq", type=str, required=False, default=None,
                           help="left (or single) fastq file")
-    
+
     required.add_argument("--right_fq", type=str, required=False, default="", #intentionally not None
                           help="right fastq file (optional)")
-    
-    optional.add_argument("--genome_lib_dir", type=str, default=os.environ.get('CTAT_GENOME_LIB'),
-                          help="genome lib directory - see http://FusionFilter.github.io for details.  Uses env var CTAT_GENOME_LIB as default")
-    
+
+    optional.add_argument("--genome", type=str, help="STAR index directory", required=True)
+
     optional.add_argument("-O", "--output_dir", type=str, required=False, default="VIF.outdir", help="output directory")
     optional.add_argument("--CPU", dest="CPU", required=False, type=int, default=4,
                           help="number of threads for multithreaded processes")
@@ -51,23 +50,23 @@ def main():
     optional.add_argument("--disable_chimeras", action='store_true', default=False,
                           help="disable chimeric read search detection")
 
-    
+
     args_parsed = arg_parser.parse_args()
-    
-    
+
+
     left_fq = os.path.abspath(args_parsed.left_fq)
     right_fq = os.path.abspath(args_parsed.right_fq) if args_parsed.right_fq else ""
     output_dir = os.path.abspath(args_parsed.output_dir)
-    genome_lib_dir = args_parsed.genome_lib_dir
+    star_genome = args_parsed.genome
     patch_db_fasta = os.path.abspath(args_parsed.patch_db_fasta)
     patch_db_gtf = os.path.abspath(args_parsed.patch_db_gtf) if args_parsed.patch_db_gtf else ""
 
     disable_chimeras_flag = args_parsed.disable_chimeras
-    
-    if not genome_lib_dir:
-        sys.stderr.write("Error, must set --genome_lib_dir or have env var CTAT_GENOME_LIB set")
+
+    if not os.path.exists(star_genome):
+        sys.stderr.write("Error, {} not found".format(star_genome))
         sys.exit(1)
-        
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -80,15 +79,15 @@ def main():
     readFilesIn = left_fq
     if right_fq:
         readFilesIn += " " + right_fq
-    
+
     cmd = " ".join(["STAR",
                     "--runThreadN {}".format(args_parsed.CPU),
-                    "--genomeDir {}".format(os.path.join(genome_lib_dir, "ref_genome.fa.star.idx")),
+                    "--genomeDir {}".format(star_genome),
                     "--outSAMtype BAM SortedByCoordinate",
                     "--twopassMode Basic",
                     "--alignSJDBoverhangMin 10",
                     "--genomeSuffixLengthMax 10000",
-                    "--limitBAMsortRAM 47271261705", 
+                    "--limitBAMsortRAM 47271261705",
                     "--alignInsertionFlush Right",
                     "--alignMatesGapMax {}".format(args_parsed.max_mate_dist),
                     "--alignIntronMax {}".format(args_parsed.max_mate_dist),
@@ -97,23 +96,23 @@ def main():
                     "--outSAMfilter KeepAllAddedReferences",
                     "--alignSJstitchMismatchNmax 5 -1 5 5",
                     "--scoreGapNoncan -6 " ])
-    
+
 
     if not disable_chimeras_flag:
         cmd += " ".join(["--chimJunctionOverhangMin 12",
                          "--chimSegmentMin 12",
                          "--chimSegmentReadGapMax 3" ])
-        
+
     if patch_db_gtf:
         cmd += " --sjdbGTFfile {} --sjdbOverhang 150 ".format(patch_db_gtf)
 
     if left_fq[-3:] == ".gz":
         cmd += " --readFilesCommand 'gunzip -c' "
 
-    
+
     pipeliner.add_commands([Command(cmd, "star_chimeric")])
-    
-    
+
+
     pipeliner.run()
 
     sys.exit(0)
