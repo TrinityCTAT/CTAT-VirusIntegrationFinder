54e32f80ea862bd995ebb2dd93e089ca49c70d4f
Brian Haas <brianjohnhaas@gmail.com>
2021-11-03 14:18:21 +0000

    updated docker for dev img testing

--- Docker/make_simg.sh
+++ Docker/make_simg.sh
@@ -4,6 +4,6 @@ VERSION=`cat VERSION.txt`
 
 singularity build -F ctat_vif.v${VERSION}.simg docker://trinityctat/ctat_vif:$VERSION
 
-singularity exec -e ctat_vif.v${VERSION}.simg ctat-VIF.py --help
+singularity exec -e ctat_vif.v${VERSION}.simg ctat-vif --help
 
 
