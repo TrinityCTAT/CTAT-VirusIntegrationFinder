c2d4169b8ae9714830d614d37849ca1597fa9b1a
Brian Haas <brianjohnhaas@gmail.com>
2021-11-19 14:04:28 -0500

    include alignment anchor length stats

--- util/incorporate_read_alignment_stats.py
+++ util/incorporate_read_alignment_stats.py
@@ -48,6 +48,7 @@ def main():
     read_to_hit_count = defaultdict(int)
     read_to_max_mismatch_count = defaultdict(int)
     read_to_max_end_clipping = defaultdict(int)
+    read_to_min_anchor_len = dict()
 
     read_counter = 0
 
@@ -61,6 +62,13 @@ def main():
 
         if (read_counter % 10000 == 0):
             logger.info("-processed {} alignments".format(read_counter))
+
+        aligned_bases = len(read.get_aligned_pairs(matches_only=True))
+        if read_name in read_to_min_anchor_len:
+            read_to_min_anchor_len[read_name] = min(aligned_bases, read_to_min_anchor_len[read_name])
+        else:
+            read_to_min_anchor_len[read_name] = aligned_bases
+
         
         NH = read.get_tag('NH')
         read_to_hit_count[read_name] = max(read_to_hit_count[read_name], NH)
@@ -80,6 +88,8 @@ def main():
             max_clip = max(max_clip, int(m.group(1)))
 
         read_to_max_end_clipping[read_name] = max(read_to_max_end_clipping[read_name], max_clip)
+
+
         
 
 
@@ -88,12 +98,14 @@ def main():
     vif_df["hits"] = ""
     vif_df["mismatches"] = ""
     vif_df["max_end_clipping"] = ""
+    vif_df["min_anchor_len"] = ""
 
     for i, row in vif_df.iterrows():
         readnames = row['readnames'].split(",")
         hits = list()
         mismatches = list()
         max_end_clipping = list()
+        min_anchor_lengths = list()
         
         for readname in readnames:
 
@@ -103,18 +115,19 @@ def main():
             hits.append(read_to_hit_count[readname])
             mismatches.append(read_to_max_mismatch_count[readname])
             max_end_clipping.append(read_to_max_end_clipping[readname])
-
+            min_anchor_lengths.append(read_to_min_anchor_len[readname])
 
         if args.detailed:
             vif_df.loc[i, 'hits'] = ",".join([str(x) for x in hits])
             vif_df.loc[i, 'mismatches'] = ",".join([str(x) for x in mismatches])
             vif_df.loc[i, 'max_end_clipping'] = ",".join([str(x) for x in max_end_clipping])
+            vif_df.loc[i, 'min_anchor_len'] = ",".join([str(x) for x in min_anchor_lengths])
 
         else:
             vif_df.loc[i, 'hits'] = "{:.3f}".format(st.mean(hits))
             vif_df.loc[i, 'mismatches'] = "{:.3f}".format(st.mean(mismatches))
             vif_df.loc[i, 'max_end_clipping'] = "{:.3f}".format(st.mean(max_end_clipping))
-
+            vif_df.loc[i, 'min_anchor_len'] = "{:.3f}".format(st.mean(min_anchor_lengths))
     
     vif_df.drop('readnames', axis=1, inplace=True)
 
