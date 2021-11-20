a2844b49e6d097a7c86530f18caa6cf544928242
Brian Haas <brianjohnhaas@gmail.com>
2021-11-20 08:50:46 -0500

    bugfix - duplicate marking wasnt removing cases where paired frags had same start coordinates

--- util/bam_mark_duplicates.py
+++ util/bam_mark_duplicates.py
@@ -23,12 +23,19 @@ def main():
 
     parser.add_argument("--remove_dups", "-r", dest='remove_dups', action='store_true', default=False,
                         help='instead of marking duplicates, just remove them')
+
+    parser.add_argument("--debug", "-d", dest='debug', action='store_true', default=False, help="debug mode")
     
     args = parser.parse_args()
 
     input_bam_filename = args.input_bam
     output_bam_filename = args.output_bam
     remove_dups_flag = args.remove_dups
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    
     
     bamreader = pysam.AlignmentFile(input_bam_filename, "rb")
 
@@ -58,6 +65,8 @@ def main():
         prev_start = -1
         prev_chrom = None
         
+
+    duplicate_counter = 0
         
     for read in bamreader.fetch():
         chrom = bamreader.get_reference_name(read.reference_id)
@@ -80,30 +89,42 @@ def main():
             # mark as duplicate
             duplicate_flag = True
             del queued_duplicate_reads[read_name]
+            logger.debug("-found paired end of duplicate")
         
-        elif mate_start > start and mate_start in current_pos_mate_coords:
+        elif mate_start >= start and mate_start in current_pos_mate_coords:
             # mark this read as a duplicate
             queued_duplicate_reads[read_name] = mate_start
             duplicate_flag = True
+            logger.debug("-marking current alignment as duplicate")
+            duplicate_counter += 1
 
         else:
             # not a duplicate
             # store mate coord in case we find others w/ similar read and mate starts
             current_pos_mate_coords.add(mate_start)
+            logger.debug("-not a duplicate here")
 
 
         # output read alignment.
         if duplicate_flag:
             read.is_duplicate = True
+            logger.debug("-setting duplicate flag")
 
         if (not duplicate_flag) or (duplicate_flag and not remove_dups_flag):
+            logger.debug("-writing record to bam")
             bamwriter.write(read)
+        else:
+            logger.debug("-not writing record to bam")
+
+        logger.debug("start: {}".format(start) + ", and current pos mate coords: {}".format(current_pos_mate_coords))
             
         prev_chrom = chrom
         prev_start = start
 
 
-    logger.info("Done.")
+
+
+    logger.info("Done. Marked {} duplicates".format(duplicate_counter))
     
     sys.exit(0)
 
