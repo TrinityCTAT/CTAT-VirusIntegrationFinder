c488e69993e356b5c53161fc2fcc143744b24d52
Brian Haas <brianjohnhaas@gmail.com>
2021-12-06 13:17:55 -0500

    adding script for extracting the human viruses from virussite.org

---
+++ util/misc/extract_human_viruses.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+
+"""
+
+Go to http://www.virusite.org/
+
+download group=human viruses as 'human_viruses.list.csv'
+
+download all viruses from donwload site, unzip, called 'genomes.fasta'
+
+use this script to extract the corresponding set of human viruses from genomes.fasta
+
+
+"""
+
+
+import sys, os, re
+import pysam
+import textwrap
+
+
+
+accs_want = set()
+with open("human_viruses.list.csv") as fh:
+    for line in fh:
+        line = line.rstrip()
+        if line == "":
+            continue
+        
+        fields = line.split(";")
+        acc_val = fields[5]
+        print(acc_val)
+        accs_want.add(acc_val)
+
+
+with pysam.FastxFile("genomes.fasta") as fh:
+    for entry in fh:
+        name = entry.name
+        acc = name.split("|")[1]
+        if acc in accs_want:
+            sequence = entry.sequence
+            sequence = textwrap.wrap(sequence, 60)
+            sequence = "\n".join(sequence).rstrip()
+            name = name.replace(" ", "_")
+
+            print(">{}\n{}".format(name, sequence))
+
+
+sys.exit(0)
+
+
+        
