6543ed8b0bee21e0b6d3eb65e731b3c97c840b2c
Brian Haas <brianjohnhaas@gmail.com>
2023-01-10 15:41:19 -0500

    add

---
+++ util/bam_remove_labeled_duplicates.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import os, sys, re
+import logging
+import argparse
+import pysam
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s : %(levelname)s : %(message)s",
+    datefmt="%H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="mark duplicates in bam",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "--input_bam",
+        "-i",
+        dest="input_bam",
+        required=True,
+        type=str,
+        help="input bam file, coordinate sorted",
+    )
+
+    parser.add_argument(
+        "--output_bam",
+        "-o",
+        dest="output_bam",
+        required=True,
+        type=str,
+        help="output bam file",
+    )
+
+    parser.add_argument(
+        "--debug",
+        "-d",
+        dest="debug",
+        action="store_true",
+        default=False,
+        help="debug mode",
+    )
+
+    args = parser.parse_args()
+
+    input_bam_filename = args.input_bam
+    output_bam_filename = args.output_bam
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    bamreader = pysam.AlignmentFile(input_bam_filename, "rb")
+
+    if ((not "SO" in bamreader.header.as_dict()["HD"])) or bamreader.header.as_dict()[
+        "HD"
+    ]["SO"] != "coordinate":
+        raise RuntimeError(
+            "Error, file: {} must be coordinate sorted".format(input_bam_filename)
+        )
+
+    bamwriter = pysam.AlignmentFile(output_bam_filename, "wb", template=bamreader)
+
+    duplicate_counter = 0
+    for read in bamreader.fetch():
+
+        if read.is_duplicate:
+            duplicate_counter += 1
+        else:
+            bamwriter.write(read)
+
+    logger.info("Done. Removed {} duplicates".format(duplicate_counter))
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
